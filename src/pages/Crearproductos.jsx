0c209dd625bd7a59e74b0319737162490b002254
FabianRG <fabian@adwise.cl>
2023-06-28 17:13:49 -0400

    fix : crear productos

--- src/pages/Crearproductos.jsx
+++ src/pages/Crearproductos.jsx
@@ -1,86 +1,88 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Form, Button } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import './ProductDetails.css';
 import TopNav from '../components/TopNav/TopNav';
 import Footer from '../components/Footer/Footer';
-import './Crearproductos.css';
 
-const CrearProducto = () => {
-  const [nombre, setNombre] = useState('');
-  const [descripcion, setDescripcion] = useState('');
-  const [precio, setPrecio] = useState('');
-  const [activo, setActivo] = useState(false);
-  const [imagen, setImagen] = useState('');
-  const [precioOferta, setPrecioOferta] = useState('');
-  const [mensaje, setMensaje] = useState('');
+const ProductDetails = () => {
+  const { id } = useParams();
+  const { products, addToCart } = useContext(AppContext); // Agrega el método addToCart del contexto
+  const [product, setProduct] = useState(null);
 
-  const handleCrearProducto = async () => {
-    try {
-      const response = await axios.post('https://backend-ecommerce-chi.vercel.app/productos/crear', {
-        nombre,
-        descripcion,
-        precio,
-        activo,
-        imagen,
-        precio_oferta: precioOferta
-      });
-      console.log(response.data.message); // Success message from the backend
-    } catch (error) {
-      console.error(error);
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://backend-ecommerce-chi.vercel.app/productos/${id}`);
+        const data = await response.json();
+        setProduct(data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const handleAddToCart = (productId) => {
+    addToCart(productId); // Llama al método addToCart del contexto para añadir el producto al carrito
   };
 
-  const handleImagenChange = (e) => {
-    setImagen(e.target.value);
+  const handleBuyNow = (productId) => {
+    // Lógica para comprar el producto
+    console.log('Comprar ahora:', productId);
   };
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <TopNav />
-      <div className="crear-producto-container">
-        <h2>Crear Producto</h2>
-        <Form>
-          <Form.Group controlId="nombre">
-            <Form.Label>Nombre:</Form.Label>
-            <Form.Control type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-          </Form.Group>
-          <Form.Group controlId="descripcion">
-            <Form.Label>Descripción:</Form.Label>
-            <Form.Control type="text" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
-          </Form.Group>
-          <Form.Group controlId="precio">
-            <Form.Label>Precio:</Form.Label>
-            <Form.Control type="number" value={precio} onChange={(e) => setPrecio(e.target.value)} />
-          </Form.Group>
-          <Form.Group controlId="activo">
-            <Form.Check
-              type="checkbox"
-              label="Activo"
-              checked={activo}
-              onChange={(e) => setActivo(e.target.checked)}
-            />
-          </Form.Group>
-          <Form.Group controlId="imagen">
-            <Form.Label>Imagen:</Form.Label>
-            <Form.Control type="text" value={imagen} onChange={handleImagenChange} />
-            {imagen && <img src={imagen} alt="Previsualización" className="imagen-previa" />}
-          </Form.Group>
-          <Form.Group controlId="precioOferta">
-            <Form.Label>Precio Oferta:</Form.Label>
-            <Form.Control type="number" value={precioOferta} onChange={(e) => setPrecioOferta(e.target.value)} />
-          </Form.Group>
-          <Button variant="primary" onClick={handleCrearProducto}>
-            Crear Producto
-          </Button>
-          {mensaje && <p className="mensaje">{mensaje}</p>}
-        </Form>
-        <Button variant="secondary" onClick={handleVolverAlHome}>
-          Volver al Home
-        </Button>
-      </div>
+      <Container className="product-detail-container">
+        <Row>
+          <Col md={8} className="product-detail-details">
+            <Row>
+              <Col md={6} xs={12} className="product-detail-image-container">
+                <Image src={product.imagen} alt={product.nombre} className="product-detail-image" fluid />
+              </Col>
+              <Col md={6} xs={12} className="product-detail-info">
+                <h3>{product.nombre}</h3>
+                <p>
+                  {product.precio_oferta ? (
+                    <>
+                      <span
+                        className={`product-detail-price-offer ${
+                          Number(product.precio_oferta) === Number(product.precio) ? 'product-detail-price-strike' : ''
+                        }`}
+                      >
+                        ${product.precio_oferta}
+                      </span>
+                      {' - '}
+                      <span className="product-detail-price">${product.precio}</span>
+                    </>
+                  ) : (
+                    <span className="product-detail-price">${product.precio}</span>
+                  )}
+                </p>
+                <p>{product.descripcion}</p>
+                {/* Resto de los campos */}
+                <Button variant="primary" onClick={() => handleAddToCart(product.id)}>
+                  Add to Cart
+                </Button>
+                <Button variant="success" onClick={() => handleBuyNow(product.id)}>
+                  Buy Now
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
 };
 
-export default CrearProducto;
+export default ProductDetails;
