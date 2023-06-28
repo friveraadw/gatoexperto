71753f8c0832928cd09dda21f9b4fc7a2846b2e7
FabianRG <fabian@adwise.cl>
2023-06-28 16:43:58 -0400

    fix: Crear Productos

--- src/pages/Crearproductos.jsx
+++ src/pages/Crearproductos.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
-import './Crearproductos.css';
+import './CrearProductos.css';
+import TopNav from '../components/TopNav/TopNav';
+import Footer from '../components/Footer/Footer';
 
 const CrearProducto = () => {
   const [nombre, setNombre] = useState('');
@@ -10,6 +12,7 @@ const CrearProducto = () => {
   const [activo, setActivo] = useState(false);
   const [imagen, setImagen] = useState('');
   const [precioOferta, setPrecioOferta] = useState('');
+  const [mensaje, setMensaje] = useState('');
 
   const handleCrearProducto = async () => {
     try {
@@ -21,49 +24,69 @@ const CrearProducto = () => {
         imagen,
         precio_oferta: precioOferta
       });
+      setMensaje('Producto creado correctamente');
       console.log(response.data.message); // Success message from the backend
     } catch (error) {
+      setMensaje('Error al crear el producto');
       console.error(error);
     }
   };
 
+  const handleVolverAlHome = () => {
+    // Implementa la lógica para volver al home aquí
+    history.push('/');
+  };
+
+  const handleImagenChange = (e) => {
+    setImagen(e.target.value);
+  };
+
   return (
-    <div className='fondo'>
-    <div className="crearproductos">
-      <h2>Crear Productos</h2>
-      <Form>
-        <Form.Group controlId="nombre">
-          <Form.Label>Nombre:</Form.Label>
-          <Form.Control type="text" value={nombre} onChange={e => setNombre(e.target.value)} />
-        </Form.Group>
-        <Form.Group controlId="descripcion">
-          <Form.Label>Descripción:</Form.Label>
-          <Form.Control type="text" value={descripcion} onChange={e => setDescripcion(e.target.value)} />
-        </Form.Group>
-        <Form.Group controlId="precio">
-          <Form.Label>Precio:</Form.Label>
-          <Form.Control type="number" value={precio} onChange={e => setPrecio(e.target.value)} />
-        </Form.Group>
-        <Form.Group controlId="activo">
-          <Form.Check
-            type="checkbox"
-            label="Activo"
-            checked={activo}
-            onChange={e => setActivo(e.target.checked)}
-          />
-        </Form.Group>
-        <Form.Group controlId="imagen">
-          <Form.Label>Imagen:</Form.Label>
-          <Form.Control type="text" value={imagen} onChange={e => setImagen(e.target.value)} />
-        </Form.Group>
-        <Form.Group controlId="precioOferta">
-          <Form.Label>Precio Oferta:</Form.Label>
-          <Form.Control type="number" value={precioOferta} onChange={e => setPrecioOferta(e.target.value)} />
-        </Form.Group>
-        <Button variant="primary" onClick={handleCrearProducto}>Crear Producto</Button>
-      </Form>
-    </div>
-    </div>
+    <>
+      <TopNav />
+      <div className="crear-producto-container">
+        <h2>Crear Producto</h2>
+        <Form>
+          <Form.Group controlId="nombre">
+            <Form.Label>Nombre:</Form.Label>
+            <Form.Control type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="descripcion">
+            <Form.Label>Descripción:</Form.Label>
+            <Form.Control type="text" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="precio">
+            <Form.Label>Precio:</Form.Label>
+            <Form.Control type="number" value={precio} onChange={(e) => setPrecio(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="activo">
+            <Form.Check
+              type="checkbox"
+              label="Activo"
+              checked={activo}
+              onChange={(e) => setActivo(e.target.checked)}
+            />
+          </Form.Group>
+          <Form.Group controlId="imagen">
+            <Form.Label>Imagen:</Form.Label>
+            <Form.Control type="text" value={imagen} onChange={handleImagenChange} />
+            {imagen && <img src={imagen} alt="Previsualización" className="imagen-previa" />}
+          </Form.Group>
+          <Form.Group controlId="precioOferta">
+            <Form.Label>Precio Oferta:</Form.Label>
+            <Form.Control type="number" value={precioOferta} onChange={(e) => setPrecioOferta(e.target.value)} />
+          </Form.Group>
+          <Button variant="primary" onClick={handleCrearProducto}>
+            Crear Producto
+          </Button>
+          {mensaje && <p className="mensaje">{mensaje}</p>}
+        </Form>
+        <Button variant="secondary" onClick={handleVolverAlHome}>
+          Volver al Home
+        </Button>
+      </div>
+      <Footer />
+    </>
   );
 };
 
